9ddc00e0654b1ef024773d2f32b4c43bb52cb7e7
K.M. Ahasan Abdullah <khanmsadim7@gmail.com>
2025-11-15 21:28:35 +0600

    24

---
+++ tarmQuestions/24/8.c
@@ -0,0 +1,1 @@
+// Sir poray nai 🙊
--- tarmQuestions/24/8a.c
+++ tarmQuestions/24/8a.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 
-struct students{
-    char studentID[6];
+struct student {
+    char studentID[10];
     char name[100];
-    char bloodGroup[2];
-    char dateOfBirth[11];
+    char bloodGroup[5];
+    char dateOfBirth[15];
 };
 
-int main()
-{
-    struct student s1;
-
-    printf("Enter number of students: ");
+int main() {
     int n;
+    printf("Enter number of students: ");
     scanf("%d", &n);
+    getchar(); // clear newline from buffer
 
-    for(int i = 0; i < n; i++)
-    {
-        printf("Enter Student ID: ");
-        gets(s1[studentID])
+    struct student s[n];
+
+    // Input
+    for (int i = 0; i < n; i++) {
+        printf("\n--- Student %d ---\n", i + 1);
+
+        printf("Student ID: ");
+        scanf("%[^\n]", &s[i].studentID);
+        getchar(); // clear newline
+
+        printf("Name: ");
+        scanf("%[^\n]", &s[i].name);
+        getchar(); // clear newline
+
+        printf("Blood Group: ");
+        scanf("%[^\n]", &s[i].bloodGroup);
+        getchar(); // clear newline
+
+        printf("Date of Birth (dd-mm-yyyy): ");
+        scanf("%[^\n]", &s[i].dateOfBirth);
+        getchar(); // clear newline
     }
+
+    // Output
+    printf("\n\n===== Student List =====\n");
+    for (int i = 0; i < n; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("ID: %s \n", s[i].studentID);
+        printf("Name: %s \n", s[i].name);
+        printf("Blood Group: %s \n", s[i].bloodGroup);
+        printf("Date of Birth: %s \n", s[i].dateOfBirth);
+    }
+
+    return 0;
 }
